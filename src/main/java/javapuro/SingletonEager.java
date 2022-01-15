72aff1e573bbb17fe1aa4b661ffb493fb10b4e86
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-15 11:14:33 -0300

    java puro - singleton

---
+++ src/main/java/javapuro/SingletonEager.java
@@ -0,0 +1,18 @@
+package javapuro;
+/**
+ * Singleton "apressado".
+ *
+ * @author SarahTumenas
+ */
+
+public class SingletonEager {
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager(){
+        super ();
+    }
+
+    public static SingletonEager getInstancia(){
+        return instancia;
+    }
+}
---
+++ src/main/java/javapuro/SingletonLazyHolder.java
@@ -0,0 +1,23 @@
+package javapuro;
+
+/**
+ * Singleton "Lazy holder"
+ *
+ * @see <a href="https://stackoverflow.com/a/24018148">Referencia</a>
+ *
+ * @author SarahTumenas
+ */
+
+public class SingletonLazyHolder {
+    private static class InstanceHolder {
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia() {
+        return InstanceHolder.instancia;
+    }
+}
